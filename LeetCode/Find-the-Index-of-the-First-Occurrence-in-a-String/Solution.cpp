3a2534a275948b522e160a0fde7a689bdab36a5b
Sapna Baranwal <128890071+Sapna-20@users.noreply.github.com>
2025-11-27 16:29:05 +0530

    commit

--- LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.cpp
+++ LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String/Solution.cpp
@@ -1,5 +1,24 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.length();
+        int m = needle.length();
+
+        if (m > n) return -1;
+
+        int i = 0, j = 0;
+
+        while (i < n) {
+            if (haystack[i] == needle[j]) {
+                i++;
+                j++;
+                if (j == m) return i - m;
+            } else {
+                i = i - j + 1;
+                j = 0;
+            }
+        }
+
+        return -1;
     }
-}
+};
