2617866c08ac76e33841f587a7c0485e38f0bd04
Sapna Baranwal <128890071+Sapna-20@users.noreply.github.com>
2025-12-20 13:28:00 +0530

    commit

--- LeetCode/Longest-Consecutive-Sequence/Solution.cpp
+++ LeetCode/Longest-Consecutive-Sequence/Solution.cpp
@@ -1,1 +1,36 @@
-[1, 2, 3, 4]
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+
+    unordered_set<int> s;
+
+    // Step 1: store all elements in HashSet
+    for (int x : nums) {
+        s.insert(x);
+    }
+
+    int longest = 0;
+
+    // Step 2: check each number
+    for (int x : s) {
+
+        // START condition: x-1 should NOT exist
+        if (s.find(x - 1) == s.end()) {
+
+            int current = x;
+            int count = 1;
+
+            // expand forward
+            while (s.find(current + 1) != s.end()) {
+                current++;
+                count++;
+            }
+
+            longest = max(longest, count);
+        }
+        // else → SKIP (do nothing)
+    }
+
+    return longest;
+}
+};
