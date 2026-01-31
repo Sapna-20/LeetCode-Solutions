e96b55f27a9ff497cda1b759490522db8773c1b9
Sapna Baranwal <128890071+Sapna-20@users.noreply.github.com>
2026-01-31 17:56:21 +0530

    Add solution for Find Peak Element

--- LeetCode/Find-Peak-Element/Solution.cpp
+++ LeetCode/Find-Peak-Element/Solution.cpp
@@ -2,29 +2,19 @@
 2public:
 3    int findPeakElement(vector<int>& nums)
 4{
-5    int left = 0;
-6    int right = nums.size() - 1;
-7
-8    while (left < right)
-9    {
-10        int mid = (left + right) / 2;
-11
-12        // if next element is bigger,
-13        // it means we are still climbing
-14        if (nums[mid] < nums[mid + 1])
-15        {
-16            left = mid + 1;
-17        }
-18        else
-19        {
-20            // here slope is going down
-21            // so peak is here or on left
-22            right = mid;
-23        }
-24    }
-25
-26    // left == right
-27    return left;
-28}
-29
-30};
+5    int l = 0, r = nums.size() - 1;
+6
+7    while (l < r)
+8    {
+9        int m = (l + r) / 2;
+10
+11        if (nums[m] < nums[m + 1])
+12            l = m + 1;
+13        else
+14            r = m;
+15    }
+16    return l;
+17}
+18
+19
+20};
