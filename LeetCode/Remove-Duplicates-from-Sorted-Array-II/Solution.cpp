a7d5d715108549c415e62d20ca7a682c14572ac0
Sapna Baranwal <128890071+Sapna-20@users.noreply.github.com>
2026-02-13 15:37:07 +0530

    Add solution for removing duplicates from sorted array
    Implement removeDuplicates function to allow at most two duplicates.

--- LeetCode/Remove-Duplicates-from-Sorted-Array-II/Solution.cpp
+++ LeetCode/Remove-Duplicates-from-Sorted-Array-II/Solution.cpp
@@ -1,1 +1,17 @@
-Input: nums = [1,1,1,2,2,3]
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 2;
+
+        if (nums.size() <= 2) return nums.size();
+        
+        for (int i = 2; i < nums.size(); i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;       
+    }
+};
