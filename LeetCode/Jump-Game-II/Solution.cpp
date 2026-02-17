6d30cb36ed1e84aa738ffe32f653ff3a1f249ecf
Sapna Baranwal <128890071+Sapna-20@users.noreply.github.com>
2026-02-17 23:20:15 +0530

    Implement jump function for Jump Game II

--- LeetCode/Jump-Game-II/Solution.cpp
+++ LeetCode/Jump-Game-II/Solution.cpp
@@ -1,9 +1,26 @@
-import random
-
-for n in range(8):
-   OUT = "["
-
-   for i in range(10 ** 4):
-       OUT += str(random.randrange(0 if n > 1 else 1, ((n+1) * 2) ** 2)) + ','
-
-   print(OUT[:-1] + "]")
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        int jumps = 0;        // number of jumps
+        int currentEnd = 0;  // end of current jump range
+        int farthest = 0;    // farthest position reachable
+        
+        // We don't need to go till last index
+        for (int i = 0; i < n - 1; i++) {
+            
+            // Update farthest reach
+            farthest = max(farthest, i + nums[i]);
+            
+            // When current range ends
+            if (i == currentEnd) {
+                jumps++;               // take a jump
+                currentEnd = farthest; // update range
+            }
+        }
+        
+        return jumps;
+    }
+};
