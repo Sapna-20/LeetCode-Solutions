fb3cae7779fc9c12df163b0eb2a288f6a0027b5d
Sapna Baranwal <128890071+Sapna-20@users.noreply.github.com>
2026-02-18 14:10:44 +0530

    Implement hIndex function to calculate H-Index

--- LeetCode/H-Index/Solution.cpp
+++ LeetCode/H-Index/Solution.cpp
@@ -1,3 +1,22 @@
-citations = [3, 0, 6, 1, 5]
-sorted = [6, 5, 3, 1, 0]
-index:   0  1  2  3  4
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        
+        int n = citations.size();
+        
+        // Sort first
+        sort(citations.begin(), citations.end());
+        
+        // Check each position
+        for (int i = 0; i < n; i++) {
+            
+            int papersLeft = n - i;
+            
+            if (citations[i] >= papersLeft) {
+                return papersLeft;
+            }
+        }
+        
+        return 0;
+    }
+};
