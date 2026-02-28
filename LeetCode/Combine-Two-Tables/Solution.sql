f019cb05c44e0b1dc190490374a88ae97ba1d946
Sapna-20 <128890071+Sapna-20@users.noreply.github.com>
2026-03-01 02:10:27 +0530

    commit

--- LeetCode/Combine-Two-Tables/Solution.sql
+++
@@ -1,9 +0,0 @@
-1SELECT 
-2    p.FirstName,
-3    p.LastName,
-4    a.City,
-5    a.State
-6FROM Person AS p
-7LEFT JOIN Address AS a
-8    ON p.PersonId = a.PersonId;
-9
--- LeetCode/arrays/Product-of-Array-Except-Self/Solution.cpp
+++ LeetCode/arrays/Product-of-Array-Except-Self/Solution.cpp
@@ -1,27 +1,28 @@
-class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-
-        int n = nums.size();
-
-        // This will store our final answer
-        vector<int> ans(n);
-
-        // Step 1: Left product
-        ans[0] = 1;
-
-        for(int i = 1; i < n; i++) {
-            ans[i] = ans[i-1] * nums[i-1];
-        }
-
-        // Step 2: Right product
-        int right = 1;
-
-        for(int i = n-1; i >= 0; i--) {
-            ans[i] = ans[i] * right;
-            right = right * nums[i];
-        }
-
-        return ans;
-    }
-};
+1class Solution {
+2public:
+3    vector<int> productExceptSelf(vector<int>& nums) {
+4
+5        int n = nums.size();
+6
+7        // This will store our final answer
+8        vector<int> ans(n);
+9
+10        // Step 1: Left product
+11        ans[0] = 1;
+12
+13        for(int i = 1; i < n; i++) {
+14            ans[i] = ans[i-1] * nums[i-1];
+15        }
+16
+17        // Step 2: Right product
+18        int right = 1;
+19
+20        for(int i = n-1; i >= 0; i--) {
+21            ans[i] = ans[i] * right;
+22            right = right * nums[i];
+23        }
+24
+25        return ans;
+26    }
+27};
+28
