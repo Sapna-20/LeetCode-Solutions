52a420d6dd4306fdbd4e2b7c59082fb804cc03d7
Sapna Baranwal <128890071+Sapna-20@users.noreply.github.com>
2026-03-23 23:39:54 +0530

    commit

--- LeetCode/sliding window/Substring-with-Concatenation-of-All-Words/Solution.cpp
+++ LeetCode/sliding window/Substring-with-Concatenation-of-All-Words/Solution.cpp
@@ -1,55 +1,55 @@
-1class Solution {
-2public:
-3    vector<int> findSubstring(string s, vector<string>& words) {
-4        
-5        vector<int> result;
-6        if (s.empty() || words.empty())
-7            return result;
-8
-9        int wordLen = words[0].size();
-10        int wordCount = words.size();
-11        
-12        unordered_map<string, int> wordMap;
-13        for (string &w : words)
-14            wordMap[w]++;
-15
-16        // Try each possible offset
-17        for (int i = 0; i < wordLen; i++) {
-18            
-19            int left = i;
-20            int count = 0;
-21            unordered_map<string, int> windowMap;
-22
-23            for (int right = i; right + wordLen <= s.size(); right += wordLen) {
-24                
-25                string word = s.substr(right, wordLen);
-26
-27                if (wordMap.count(word)) {
-28                    
-29                    windowMap[word]++;
-30                    count++;
-31
-32                    // If extra word, shrink window
-33                    while (windowMap[word] > wordMap[word]) {
-34                        string leftWord = s.substr(left, wordLen);
-35                        windowMap[leftWord]--;
-36                        left += wordLen;
-37                        count--;
-38                    }
-39
-40                    // If full match
-41                    if (count == wordCount)
-42                        result.push_back(left);
-43
-44                } else {
-45                    // Reset if invalid word
-46                    windowMap.clear();
-47                    count = 0;
-48                    left = right + wordLen;
-49                }
-50            }
-51        }
-52
-53        return result;
-54    }
-55};
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        
+        vector<int> result;
+        if (s.empty() || words.empty())
+            return result;
+
+        int wordLen = words[0].size();
+        int wordCount = words.size();
+        
+        unordered_map<string, int> wordMap;
+        for (string &w : words)
+            wordMap[w]++;
+
+        // Try each possible offset
+        for (int i = 0; i < wordLen; i++) {
+            
+            int left = i;
+            int count = 0;
+            unordered_map<string, int> windowMap;
+
+            for (int right = i; right + wordLen <= s.size(); right += wordLen) {
+                
+                string word = s.substr(right, wordLen);
+
+                if (wordMap.count(word)) {
+                    
+                    windowMap[word]++;
+                    count++;
+
+                    // If extra word, shrink window
+                    while (windowMap[word] > wordMap[word]) {
+                        string leftWord = s.substr(left, wordLen);
+                        windowMap[leftWord]--;
+                        left += wordLen;
+                        count--;
+                    }
+
+                    // If full match
+                    if (count == wordCount)
+                        result.push_back(left);
+
+                } else {
+                    // Reset if invalid word
+                    windowMap.clear();
+                    count = 0;
+                    left = right + wordLen;
+                }
+            }
+        }
+
+        return result;
+    }
+};
